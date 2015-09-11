72aad064cdcf6aa57d711f8927e8c93912b2dfbd
Colin Sherratt <colin.sherratt@gmail.com>
2015-09-11 02:12:08 -0400

    Added HairballReader

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate capnp;
 extern crate uuid;
 
-use std::rc::Rc;
 use std::collections::{HashMap, HashSet};
 
 pub mod hairball_capnp {
@@ -12,48 +11,9 @@ const MAJOR: &'static str = env!("CARGO_PKG_VERSION_MAJOR");
 const MINOR: &'static str = env!("CARGO_PKG_VERSION_MINOR");
 const PATCH: &'static str = env!("CARGO_PKG_VERSION_PATCH");
 
-pub enum Name {
-    Path(String),
-    Index(u32)
-}
-
-pub enum Entity {
-    Local(LocalEntity),
-    External(ExternalEntity)
-}
-
-impl Entity {
-    fn write(&self, mut builder: hairball_capnp::entity::Builder)  {
-        match *self {
-            Entity::Local(ref e) => {
-                e.write(builder.init_local())
-            }
-            Entity::External(ref e) => {
-                e.write(builder.init_external())
-            }
-        }
-    }
-}
-
-pub struct LocalEntity {
-    name: Option<String>,
-    parent: Option<u32>
-}
-
-pub struct ExternalEntity {
-    file: uuid::Uuid,
-    name: Name
-}
-
-impl ExternalEntity {
-    fn write(&self, mut builder: hairball_capnp::external_entry::Builder) {
-
-    }
-}
-
 pub struct HairballBuilder {
     uuid: uuid::Uuid,
-    entity: Vec<Rc<Entity>>,
+    entity: Vec<Entity<String>>,
     builder: capnp::message::Builder<capnp::message::HeapAllocator>
 }
 
@@ -80,14 +40,14 @@ impl HairballBuilder {
     }
 
     /// Adds a local entity to the file's keyspace
-    pub fn add_entity(&mut self, entry: LocalEntity) -> u32 {
-        self.entity.push(Rc::new(Entity::Local(entry)));
+    pub fn add_entity(&mut self, entry: LocalEntity<String>) -> u32 {
+        self.entity.push(Entity::Local(entry));
         self.entity.len() as u32 - 1
     }
 
     /// Adds a external entity to the file's key space
-    pub fn add_external_entity(&mut self, entry: ExternalEntity) -> u32 {
-        self.entity.push(Rc::new(Entity::External(entry)));
+    pub fn add_external_entity(&mut self, entry: ExternalEntity<String>) -> u32 {
+        self.entity.push(Entity::External(entry));
         self.entity.len() as u32 - 1
     }
 
@@ -123,9 +83,15 @@ impl HairballBuilder {
     }
 }
 
-impl LocalEntity {
+#[derive(Debug)]
+pub struct LocalEntity<T> {
+    name: Option<T>,
+    parent: Option<u32>
+}
+
+impl<T> LocalEntity<T> {
     /// Create a new Entity with the supplied name
-    pub fn named(name: String) -> LocalEntity {
+    pub fn named(name: T) -> LocalEntity<T> {
         LocalEntity {
             name: Some(name),
             parent: None
@@ -133,7 +99,7 @@ impl LocalEntity {
     }
 
     /// Creates an anonymous entity 
-    pub fn anonymous() -> LocalEntity {
+    pub fn anonymous() -> LocalEntity<T> {
         LocalEntity {
             name: None,
             parent: None
@@ -141,13 +107,15 @@ impl LocalEntity {
     }
 
     /// Set a parent to this object
-    pub fn parent(self, parent: u32) -> LocalEntity {
+    pub fn parent(self, parent: u32) -> LocalEntity<T> {
         LocalEntity {
             parent: Some(parent),
             name: self.name,
         }
     }
+}
 
+impl LocalEntity<String> {
     fn write(&self, mut builder: hairball_capnp::local_entry::Builder) {
         if let Some(ref name) = self.name {
             builder.set_name(&name[..])
@@ -155,3 +123,112 @@ impl LocalEntity {
         builder.set_parent(if let Some(id) = self.parent { id } else { !0 });
     }
 }
+
+impl<'a> LocalEntity<&'a str> {
+    fn read(reader: hairball_capnp::local_entry::Reader<'a>) -> Result<LocalEntity<&'a str>, capnp::Error> {
+        Ok(LocalEntity {
+            name: if reader.has_name() {
+                Some(try!(reader.get_name()))
+            } else {
+                None
+            },
+            parent: match reader.get_parent() {
+                0xffffffff => None,
+                x => Some(x)
+            }
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum Entity<T> {
+    Local(LocalEntity<T>),
+    External(ExternalEntity<T>)
+}
+
+impl<'a> Entity<&'a str> {
+    pub fn name(&self) -> Option<&str> {
+        match *self {
+            Entity::Local(ref e) => e.name,
+            Entity::External(ref e) => Some(&e.name)
+        }
+    }
+}
+
+impl Entity<String> {
+    fn write(&self, mut builder: hairball_capnp::entity::Builder)  {
+        match *self {
+            Entity::Local(ref e) => {
+                e.write(builder.init_local())
+            }
+            Entity::External(ref e) => {
+                e.write(builder.init_external())
+            }
+        }
+    }
+}
+
+impl<'a> Entity<&'a str> {
+    fn read(e: hairball_capnp::entity::Reader<'a>) -> Result<Entity<&'a str>, capnp::Error> {
+        use hairball_capnp::entity::Which;
+
+        Ok(match try!(e.which()) {
+            Which::Local(l) => {
+                Entity::Local(try!(LocalEntity::read(try!(l))))
+            }
+            Which::External(e) => {
+                Entity::External(try!(ExternalEntity::read(try!(e))))
+            }
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct ExternalEntity<T> {
+    file: uuid::Uuid,
+    name: T
+}
+
+impl<T> ExternalEntity<T> {
+    fn write(&self, mut builder: hairball_capnp::external_entry::Builder) {
+
+    }
+
+    fn read(reader: hairball_capnp::external_entry::Reader) -> Result<ExternalEntity<T>, capnp::Error> {
+        panic!()
+    }
+}
+
+pub struct HairballReader {
+    reader: capnp::message::Reader<capnp::serialize::OwnedSegments>,
+}
+
+impl HairballReader {
+    pub fn read<R>(r: &mut R) -> Result<HairballReader, capnp::Error>
+        where R: std::io::Read
+    {
+        let opts = capnp::message::ReaderOptions::new();
+        capnp::serialize::read_message(r, opts)
+            .map(|r| HairballReader{reader: r})
+    }
+
+    pub fn entities_len(&self) -> usize {
+        self.reader.get_root::<hairball_capnp::hairball::Reader>()
+            .and_then(|root| root.get_entities()).ok()
+            .map(|x| x.len() as usize)
+            .unwrap_or(0)
+    }
+
+    pub fn entity(&self, idx: usize) -> Option<Entity<&str>> {
+        self.reader.get_root::<hairball_capnp::hairball::Reader>()
+            .and_then(|root| root.get_entities()).ok()
+            .and_then(|entities| {
+                if (entities.len() as usize) < idx {
+                    None
+                } else {
+                    Entity::read(entities.get(idx as u32)).ok()
+                }
+            })
+    }
+}
+
--- tests/test.rs
+++ tests/test.rs
@@ -1,19 +1,30 @@
 extern crate hairball;
 
-use hairball::{HairballBuilder, LocalEntity};
+use hairball::{HairballReader, HairballBuilder, LocalEntity};
+
 
 #[test]
-fn build_file() {
-	let mut hairball = HairballBuilder::new();
+fn file_eid_0_to_10_write() {
+    let mut hairball = HairballBuilder::new();
 
-	for i in 0..4 {
-		hairball.add_entity(LocalEntity::anonymous());
-	}
+    for i in 0..10 {
+        hairball.add_entity(
+            LocalEntity::named(format!("{}", i))
+        );
+    }
 
-	hairball.add_entity(LocalEntity::named("Kitten".to_string()));
+    let mut vec: Vec<u8> = Vec::new();
+    hairball.write(&mut vec);
+}
 
-    let mut file = std::fs::File::create("test.hairball").unwrap();
-    hairball.write(&mut file);
-
-
+#[test]
+fn file_eid_0_to_10_read() {
+    let mut file = std::fs::File::open("hairballs/ref/0-10.hairball").unwrap();
+    let mut hairball = HairballReader::read(&mut file).unwrap();
+    
+    assert_eq!(hairball.entities_len(), 10);
+    for i in 0..hairball.entities_len() {
+        let e = hairball.entity(i).unwrap();
+        assert_eq!(format!("{}", i), e.name().unwrap())
+    }
 }
